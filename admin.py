4378b4ae05b229bfa37c745de72377b0584a3535
Philip V <philiplugt@netscape.net>
2023-08-27 08:19:41 +0200

    Update admin.py
    Updated the UserAdmin

--- admin.py
+++ admin.py
@@ -1,9 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth import admin as auth_admin
 from django.contrib.auth.models import Group as OldGroup
+from django.utils.translation import gettext_lazy as _
+
 from .models import User, Group
 
 # Register your models here.
 admin.site.unregister(OldGroup)
-admin.site.register(User)
 admin.site.register(Group)
 
+# Register user and custom admin panel
+@admin.register(User)
+class UserAdmin(auth_admin.UserAdmin):
+    list_display = ("email", "username", "date_joined", "is_staff")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("-date_joined",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("username", "first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
